14ac7caf0997537b9c79bab71072f33d7d55505b
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2024-02-28 18:16:16 +0530

    Update tut_7.java

---
+++ tut_7.java
@@ -0,0 +1,23 @@
+public class tut_7 {
+    public int[] searchRange(int[] nums, int target) {
+        int first=-1;
+        int last=-1;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                if(first == -1)
+                {
+                    first =i;
+                }
+                last=i;
+            }
+        }
+        return new int[]{first,last};
+        }
+
+        
+
+        
+    }
