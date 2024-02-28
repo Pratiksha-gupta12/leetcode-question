975829254c1d3f1ffc2930f4f77444d0667cd7b5
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2024-02-28 18:17:24 +0530

    Update tut_6.java

---
+++ tut_6.java
@@ -0,0 +1,13 @@
+public class tut_6 {
+    public int[] findArray(int[] pref) {
+        int [] arr=new int[pref.length];
+        arr[0]=pref[0];
+
+        for(int i=1;i<pref.length;i++)
+        {
+            arr[i]=pref[i-1]^pref[i];
+        }
+        return arr;
+        
+    }
+} 
